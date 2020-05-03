a812a5742e274a33038c92bbc44723c84a7fa902
Simran jeet Singh <simranj674@gmail.com>
2020-05-03 23:30:29 +0530

    Updates

---
+++ precache-manifest.154edbb42621a05f1221a72b78d06ad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1718dce909896973c3b44a0d6b153658",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "cdd78427999c6055f0da",
+    "url": "/CoronaTracker/static/css/main.a93d8fef.chunk.css"
+  },
+  {
+    "revision": "08dfb7824f8e4a7157ec",
+    "url": "/CoronaTracker/static/js/2.8308a862.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoronaTracker/static/js/2.8308a862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd78427999c6055f0da",
+    "url": "/CoronaTracker/static/js/main.fc842beb.chunk.js"
+  },
+  {
+    "revision": "3b21706eb87213cc1fd4",
+    "url": "/CoronaTracker/static/js/runtime-main.5a10aa53.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoronaTracker/static/media/COVID-19.d7265326.jpg"
+  }
+]);
